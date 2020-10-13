5a07b62a1a218e657b2931b725b0dba1c33d685c
Tom Marto <tommartocoding@gmail.com>
2020-10-13 12:45:37 -0300

    Web Drums V1.2 refactor js code
    refactor js code on app.js file.
    Change the position of the transitionend event listener.

--- app.js
+++ app.js
@@ -2,15 +2,18 @@
 addEventListener('keydown',(event)=>{
     let audio   =   document.querySelector(`audio[data-key="${event.keyCode}"]`);
     let key     =   document.querySelector(`.keys__key[data-key="${event.keyCode}"]`);
-    
-    if(!audio) return;
 
+    if(!audio) return;
+    
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-    key.addEventListener('transitionend',()=>{
-        key.classList.remove('playing');
-    });
+    
+});
+
+let keys    =   document.querySelector('.keys');
+keys.addEventListener('transitionend',(event)=>{
+    event.target.classList.remove('playing');
 });
 
 
