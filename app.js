cd43c4ce4f1745a39360a31348912875eae57723
Tom Marto <tommartocoding@gmail.com>
2020-10-13 12:36:31 -0300

    App Working  Version 1.0
    add app.js code, still can refactor somethings but it's working.

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-/*
+
 addEventListener('keydown',(event)=>{
-    console.log(event);
+    let audio   =   document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    let key     =   document.querySelector(`.keys__key[data-key="${event.keyCode}"]`);
+    
+    if(!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+    key.addEventListener('transitionend',()=>{
+        key.classList.remove('playing');
+    });
 });
-*/
+
+
